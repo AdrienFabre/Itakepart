d8a44a71155bb8ec3682f76269bb8b913cca5d5d
Adrien Fabre <adrien.fabre.1@gmail.com>
2020-11-11 22:13:36 +0100

    refine basic design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './Itakepart-Logo-Improved-300x300.png';
+import logo from './Itakepart-Logo-Improved-768x771.png';
 import './App.css';
 
 function App() {
@@ -11,72 +11,81 @@ function App() {
         I take part is a social innovation lab.
         <br/>
         We cultivate social impact leadership.
-        <br/>
-        <br/>
-        Coming soon.
         </p>
+        </header>
+        <div>
 
         <div className='layer2'>
-        <p>We explore and experiment ways to create social impact ecosystems, supporting individuals and organisations to take actions, to collaborate towards their personal and collective fulfilment.</p>
-        <p>We facilitate co-creation, producing playful, inspirational, educational and entrepreneurial experiences,  through events and programmes using personal development and design thinking methodologies.</p>
-        <p>
-        We foster diversity and inclusion to nurture innovation through focusing on the familiar, common and urgent challenge of food sovereignty, building sustainable systems and circular economies.</p>
+          <div className='text'>
+          <p>We explore and experiment ways to create social impact ecosystems, supporting individuals and organisations to take actions, to collaborate towards their personal and collective fulfilment.</p>
+          <p>We facilitate co-creation, producing playful, inspirational, educational and entrepreneurial experiences,  through events and programmes using personal development and design thinking methodologies.</p>
+          <p>
+          We foster diversity and inclusion to nurture innovation through focusing on the familiar, common and urgent challenge of food sovereignty, building sustainable systems and circular economies.</p>
+          </div>
         </div>
 
         <div className='layer3'>
-        <p>Know where to start.</p>
-        <p>
-        Understand and connect with the social innovation and food sustainability ecosystems.
-        </p>
-        <p>
-        Develop skills to strive.
-        </p>
-        <p>
-        Practice problem solving, system mapping, collaboration and social entrepreneurship.
-        </p>
-        <p>
-        Nurture relationships to grow.
-        </p>
-        <p>
-        Meet experts and partners to increase your abilities to shape the future together.
-        </p>
-        <p>
-        Create a movement.
-        </p>
-        <p>
-        Lead the change using your strengths and network to scale and spread your impact.
-        </p>
-        </div>
+          <div className='text'>
+          <p>Know where to start.
+          <br/>
+          Understand and connect with the social innovation and food sustainability ecosystems.
+          </p>
+          <p>
+          Develop skills to strive.
+          <br/>
+          Practice problem solving, system mapping, collaboration and social entrepreneurship.
+          </p>
+          <p>
+          Nurture relationships to grow.
+          <br/>
+          Meet experts and partners to increase your abilities to shape the future together.
+          </p>
+          <p>
+          Create a movement.
+          <br/>
+          Lead the change using your strengths and network to scale and spread your impact.
+          </p>
+          </div>
+        </div> 
+
 
       <div className='layer2'>
-        <p> “If not us, then who? If not now, then when?” — John Lewis
-        </p>
-        <p> 
-        Take Part, Start Here.
-        </p>
-      </div>
+        <div className='text'>
+          <p> “If not us, then who? If not now, then when?” — John Lewis
+          </p>
+          <p> 
+          Take Part, Start Here.
+          </p>
+        </div>
+      </div> 
+
 
 
       <div className='layer3'>
-        <p>Adrien has always been creative, willing to fix things. 10 years ago, He was living in a small town in southern France, studying vocational electronics, working in food processing factories and fast-foods during his time off. He found inconsistency in the world, he was curious to understand. 
-</p>
-        <p>He got a Master in Business Engineering, learned languages, spent time in Australia, India and Russia, went through purchasing, sales, project management experiences and decided to settle in London and learn from worldwide experts. 
-</p>
-        <p>He explored the social innovation and food sustainability ecosystem for over 2 years, attending hundreds of events and creating his own, trying to find ways to create social impact, to raise awareness and encourage actions about global challenges. 
-</p>
-        <p>He excels at initiating and developing relationships, creating and facilitating action learning events, collaborative tools, and processes. He decided to create I take part, a social innovation lab helping individuals and organisations to lead social impact.
-</p>
-      </div>
-
-<div className='layer2'>
-  <p>
-  “It was a delightful working with Adrien. He produced a highly engaging and well attended MakeSense workshop for Chatterbox in very little time. He was very attentive to our needs and delivered outputs that far exceeded our expectations. I would highly recommend him.”
-</p><p>
-Mursal Hedayat, CEO at Chatterbox, FounderForbes 30 Under 30 Social Entrepreneur
+        <div className='text'>
+          <p>Adrien has always been creative, willing to fix things. 10 years ago, He was living in a small town in southern France, studying vocational electronics, working in food processing factories and fast-foods during his time off. He found inconsistency in the world, he was curious to understand. 
   </p>
-  </div> 
+          <p>He got a Master in Business Engineering, learned languages, spent time in Australia, India and Russia, went through purchasing, sales, project management experiences and decided to settle in London and learn from worldwide experts. 
+  </p>
+          <p>He explored the social innovation and food sustainability ecosystem for over 2 years, attending hundreds of events and creating his own, trying to find ways to create social impact, to raise awareness and encourage actions about global challenges. 
+  </p>
+          <p>He excels at initiating and developing relationships, creating and facilitating action learning events, collaborative tools, and processes. He decided to create I take part, a social innovation lab helping individuals and organisations to lead social impact.
+  </p>
+        </div>
+      </div> 
 
-      </header>
+
+      <div className='layer2'>
+        <div className='text'>
+
+          <p>
+          “It was a delightful working with Adrien. He produced a highly engaging and well attended MakeSense workshop for Chatterbox in very little time. He was very attentive to our needs and delivered outputs that far exceeded our expectations. I would highly recommend him.”
+        </p><p>
+        Mursal Hedayat, CEO at Chatterbox, FounderForbes 30 Under 30 Social Entrepreneur
+          </p>
+        </div> 
+      </div> 
+      </div>
     </div>
   );
 }
